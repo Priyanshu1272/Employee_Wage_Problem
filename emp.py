193fc1fa808185aa2a1f2a682f2f8641d2a9e613
Priyanshu <priyanshu1272.be21@chitkarauniversity.edu.in>
2025-02-16 23:48:53 +0530

    [Priyanshu] Add:Python script to check daily wage of an employee

--- emp.py
+++ emp.py
@@ -1,25 +1,35 @@
 import random
 
-
 def check_attendance():
-    """
-    Function to check the attendance of an employee.
-    It randomly determines whether the employee is present or absent.
-    """
-    print("Welcome to Employee Wages Computation Program on Master Branch")   
-    attendance = random.choice([1, 0])  # Randomly selects 1 (Present) or 0 (Absent)
-    if attendance == 1:
-        print("The employee is Present")
-    else:
-        print("The employee is Absent")
+	"""
+	Check employee attendance.
+	This function randomly determines whether the employee is present (1) or absent (0).
+	"""
 
-if __name__ == "__main__":
-    """
-    Main entry point of the program.
-    Calls the check_attendance function and prints a welcome message.
-    """
-    check_attendance()
-    print("Welcome To Employee Wage Computation")
+	print("Welcome to Employee Wages Computation Program on Master Branch")
+	attendance = random.choice([1, 0])
+	return attendance
+
+def emp_daily_wage():
+	"""
+	Calculate and display the employee daily wage.
+	This function checks the employee attendance and calculates their daily wage 
+	based on a fixed hourly rate. If the employee is present, the wage is calculated 
+	for 8 hours; otherwise, the wage is 0.
+	"""
+	
+	wage_per_hr = 20
+	emp_check = check_attendance()
+	if emp_check == 1:
+		daily_wage = wage_per_hr * 8
+		print(f"The Employee is present for full day so the daily wage is : {daily_wage}")
+	else:
+		daily_wage = 0
+		print(f"Employee is absent for the day so the daily wage is: {daily_wage}")
+
+if __name__=="__main__":
+	emp_daily_wage()
+
 
 
 
